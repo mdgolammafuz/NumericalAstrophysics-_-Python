95c6607dbbcf4704825efc3680ccf3b166d4c473
mdgolammafuzgm <golam.mafuz.work2@gmail.com>
2023-08-08 20:02:38 +0530

    Perfecting Mathematics with Python

--- Numpy/numpy_excercise.py
+++ Numpy/numpy_excercise.py
@@ -5,7 +5,7 @@ def getArray():
    # Generating vector array
     vector_array = np.arange(25)
     # reshape to convert into 2-d
-    matrix_array = vector_array.reshape(5, 5)
+    matrix_array = vector_array.reshape(5,5)
     # Scalar addition to the NumPy array
     return (matrix_array + 100)
 
--- little_tricks_and_concepts/double_arg.py
+++ little_tricks_and_concepts/double_arg.py
@@ -2,4 +2,4 @@ def save_user(**user):
     print(user)
 
 
-save_user(id=1, name="Mafuz", age="22")  # will print a dictionary
+save_user(id=1,name="Mafuz",age="22")  # will print a dictionary
